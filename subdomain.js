0f09e225a8a6738085c2b0d32f4085d75669663b
cth-inni <charlie.tang@innity.com>
2020-03-24 20:16:58 +0800

    Test Json

---
+++ subdomain.js
@@ -0,0 +1,19 @@
+{
+    "info": {
+        "version"  : "1.0",
+        "updated"  : "2020-03-24T12:13:16.022Z",
+        "sheetName": "Subdomain",
+        "rowCount" : 8,
+        "keys"     : []
+    },
+    "results": [{
+        "acar"    : "https://sites.google.com/view/acar-my",
+        "btsb"    : "https://sites.google.com/view/btsb",
+        "gomedia" : "https://sites.google.com/view/gomedia",
+        "mysecure": "https://sites.google.com/view/btsb/mysecure",
+        "mytrack" : "https://sites.google.com/view/mytrack-malaysia",
+        "net2u"   : "http://sites.google.com/view/net2u",
+        "seca"    : "https://seca.org.my/",
+        "secacom" : "https://seca.com.my/"
+    }]
+}
