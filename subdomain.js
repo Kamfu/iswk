c02f8cf38ef88453c224d78696da9a60ce34fc22
cth-inni <charlie.tang@innity.com>
2020-03-24 20:32:19 +0800

    add subdomain

--- subdomain.js
+++ subdomain.js
@@ -14,6 +14,7 @@
         "mytrack" : "https://sites.google.com/view/mytrack-malaysia",
         "net2u"   : "http://sites.google.com/view/net2u",
         "seca"    : "https://seca.org.my/",
-        "secacom" : "https://seca.com.my/"
+        "secacom" : "https://seca.com.my/",
+        "devto"   : "https://dev.to/"
     }]
 }
